c0b7aaae7e03a3233507caf2914372a178bd949d
Alexander Sloan <alexander.charles.sloan@gmail.com>
2019-10-28 10:57:19 -0600

    encrypted credentials

--- app/src/main/java/com/msd/lifestyleapp/controller/MainActivity.java
+++ app/src/main/java/com/msd/lifestyleapp/controller/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
@@ -18,10 +20,26 @@ import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.msd.lifestyleapp.R;
+import com.msd.lifestyleapp.model.UserRoomDatabase;
 import com.msd.lifestyleapp.model.UserViewModel;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -49,6 +67,11 @@ public class MainActivity extends AppCompatActivity {
     private boolean usersExist;
     private static final String LOG_TAG = MainActivity.class.getName();
 
+    private static final String encryptionKey           = "ABCDEFGHIJKLMNOP";
+    private static final String characterEncoding       = "UTF-8";
+    private static final String cipherTransformation    = "AES/CBC/PKCS5PADDING";
+    private static final String aesEncryptionAlgorithem = "AES";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,29 +81,82 @@ public class MainActivity extends AppCompatActivity {
         // Get a new or existing ViewModel from the ViewModelProvider.
         userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
         AWSMobileClient.getInstance().initialize(this).execute();
-//        uploadWithTransferUtility();
+        uploadWithTransferUtility();
 //        downloadWithTransferUtility();
+    }
 
+    public static String decrypt(String encryptedText) {
+        String decryptedText = "";
+        try {
+            Cipher cipher = Cipher.getInstance(cipherTransformation);
+            byte[] key = encryptionKey.getBytes(characterEncoding);
+            SecretKeySpec secretKey = new SecretKeySpec(key, aesEncryptionAlgorithem);
+            IvParameterSpec ivparameterspec = new IvParameterSpec(key);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivparameterspec);
+            Base64.Decoder decoder = Base64.getDecoder();
+            byte[] cipherText = decoder.decode(encryptedText.getBytes("UTF8"));
+            decryptedText = new String(cipher.doFinal(cipherText), "UTF-8");
+
+        } catch (Exception E) {
+            System.err.println("decrypt Exception : "+E.getMessage());
+        }
+        return decryptedText;
+    }
+
+
+
+    public ArrayList<String> readFile(){
+
+        ArrayList<String> res = new ArrayList<>();
+
+        StringBuilder text = new StringBuilder();
+
+        try {
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(getResources().openRawResource(
+                            getResources().getIdentifier("credentials",
+                                    "raw", getPackageName()))));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                res.add(line);
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //You'll need to add proper error handling here
+            System.out.println("Unable to read file");
+        }
+
+        System.out.println("TEXT: " + text);
+        return res;
     }
 
     public void uploadWithTransferUtility() {
 
-//        String KEY = "AKIATX7DK4M6QDYYMQWI";
-//        String SECRET = "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";
-//        BasicAWSCredentials credentials = new BasicAWSCredentials(KEY,SECRET);
-//        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        ArrayList<String> creds = readFile();
+
+        String KEY = decrypt(creds.get(0));
+        String SECRET = decrypt(creds.get(1));
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(KEY,SECRET);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
 
         TransferUtility transferUtility =
                 TransferUtility.builder()
                         .context(getApplicationContext())
                         .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .s3Client(s3Client)
                         .build();
+
+        //unique device id to name the file
+        String androidId = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
 
         TransferObserver uploadObserver =
                 transferUtility.upload(
-                        "s3Folder/s3Key.txt",
-                        new File("/path/to/file/localFile.txt"));
+                        "s3Folder/" + androidId,
+                        new File( "/data/data/com.msd.lifestyleapp/databases/awss3transfertable.db"));
 
         // Attach a listener to the observer to get state update and progress notifications
         uploadObserver.setTransferListener(new TransferListener() {
@@ -120,22 +196,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void downloadWithTransferUtility() {
 
-//        String KEY = "AKIATX7DK4M6QDYYMQWI";
-//        String SECRET = "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";
-//        BasicAWSCredentials credentials = new BasicAWSCredentials(KEY,SECRET);
-//        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+        ArrayList<String> creds = readFile();
+
+        String KEY = decrypt(creds.get(0));
+        String SECRET = decrypt(creds.get(1));
+
+        BasicAWSCredentials credentials = new BasicAWSCredentials(KEY,SECRET);
+        AmazonS3Client s3Client = new AmazonS3Client(credentials);
 
         TransferUtility transferUtility =
                 TransferUtility.builder()
                         .context(getApplicationContext())
                         .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
+                        .s3Client(s3Client)
                         .build();
+
+        //unique device id to name the file
+        String androidId = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
 
         TransferObserver downloadObserver =
                 transferUtility.download(
-                        "s3Folder/s3Key.txt",
-                        new File("/Users/alexcsloan/Desktop/LifestyleAppV2/app/src/main/res/raw/localFile.txt"));
+                        "s3Folder/" + androidId,
+                        new File( "/data/data/com.msd.lifestyleapp/databases/awss3transfertable.db"));
 
         // Attach a listener to the observer to get state update and progress notifications
         downloadObserver.setTransferListener(new TransferListener() {
